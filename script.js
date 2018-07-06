c190b605655bbd6e25d90c8e617502066cecc0e5
Jacek Jackiewicz <jjackiewicz86@gmail.com>
2018-07-06 09:00:44 +0200

    Script.js changes added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var CounterOne = React.createClass({
+var Counter = React.createClass({
     getInitialState: function() {
         return {
             counter: 0
@@ -37,39 +37,16 @@ var CounterOne = React.createClass({
         return React.createElement('div', {},
             React.createElement('button', {onClick: this.increment}, '+1'),
             React.createElement('button', {onClick: this.decrement}, '-1'),
-			React.createElement('span', {}, 'Result: ' + this.state.counter)
+            React.createElement('span', {}, 'Result: ' + this.state.counter)
         );
     },
     componentWillUnmount: function () {
         console.log('Metoda wywoływana po usunięciu komponentu z drzewa DOM.');
     }
 });
-var CounterTwo = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 5
-        });
-    },
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 3
-        });
-    },
-    render: function() {
-        return React.createElement('div', {},
-            React.createElement('button', {onClick: this.increment}, '+5'),
-            React.createElement('button', {onClick: this.decrement}, '-3'),
-			React.createElement('span', {}, 'Result: ' + this.state.counter)
-        );
-	},
-});
-var element = React.createElement('div', {}, 
-    React.createElement(CounterOne), 
-    React.createElement(CounterTwo));
 
+var counterOne = React.createElement(Counter);
+var counterTwo = React.createElement(Counter);
+
+var element = React.createElement('div', {}, counterOne, counterTwo);
 ReactDOM.render(element, document.getElementById('app'));
